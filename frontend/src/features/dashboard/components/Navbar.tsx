9d278b50aacafd5a522ff3d56675e50e9bd1e130
AyushSharma72G <ayush.sharma@gammaedge.io>
2026-04-16 11:36:11 +0530

    fix(cu-86d2nvfua):added real project data in the dashboard page and fixed ui

--- frontend/src/features/dashboard/components/Navbar.tsx
+++ frontend/src/features/dashboard/components/Navbar.tsx
@@ -4,32 +4,30 @@ import { useAppSelector } from "../../../store/hooks";
 import { selectAuthUser } from "../../auth/store/authSelectors";
 import PrimaryButton from "../../../shared/components/buttons/PrimaryButton";
 
+const Welcome = () => {
+  const navigate = useNavigate();
+  const user = useAppSelector(selectAuthUser);
+  console.log(user);
 
-const Welcome = ()=>{
+  const profileName = user?.name || "Learner";
 
-    const navigate = useNavigate()
-    const user = useAppSelector(selectAuthUser);
-    console.log(user);
-
-    const profileName = user?.name || 'Learner';
-
-    return(
-        <div className="flex justify-between ">
-        <div>
-            <h1 className="font-[var(--font-weight-heading)] text-xl">
-            Welcome back, {profileName}!👋 
-            </h1>
-           <p className="text-[length:var(--text-description)] font-[var(--font-weight-description)]">
-            Here's what's happening with your projects today.
-            </p>
-        </div>
-           <div>
-            <PrimaryButton onClick={()=>navigate('/projects')}  icon={<GoPlus />}>
-            New Project
-          </PrimaryButton>
-           </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-between mb-3">
+      <div className="flex flex-col gap-2">
+        <h1 className="font-[var(--font-weight-heading)] text-xl">
+          Welcome back, {profileName}!👋
+        </h1>
+        <p className="text-[length:var(--text-description)] font-[var(--font-weight-description)]">
+          Here's what's happening with your projects today.
+        </p>
+      </div>
+      <div>
+        <PrimaryButton onClick={() => navigate("/projects")} icon={<GoPlus />}>
+          New Project
+        </PrimaryButton>
+      </div>
+    </div>
+  );
+};
 
 export default Welcome;
--- frontend/src/features/dashboard/pages/DashboardPage.tsx
+++ frontend/src/features/dashboard/pages/DashboardPage.tsx
@@ -1,21 +1,19 @@
-
 import Welcome from "../components/Navbar";
 import ProgressBar from "../components/ProgressBar";
 import ProjectBar from "../components/ProjectBar";
 import RecentActivity from "../components/RecentActivity";
 
-const DashboardPage = ()=>{
-    
-    return(
-        <div  className="bg-[var(--color-bg)] pt-3">
-           <Welcome/>
-           <ProgressBar/>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                <ProjectBar/> 
-           <RecentActivity/>
-           </div>
-        </div>
-    )
-}
+const DashboardPage = () => {
+  return (
+    <div className="bg-[var(--color-bg)] pt-3">
+      <Welcome />
+      <ProgressBar />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+        <ProjectBar />
+        <RecentActivity />
+      </div>
+    </div>
+  );
+};
 
 export default DashboardPage;
