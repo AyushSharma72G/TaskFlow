5f3af4baa93afb9222fb0fd3aee27c052503466b
AyushSharma72G <ayush.sharma@gammaedge.io>
2026-04-16 15:27:09 +0530

    chnaged cors temperorily for deployment

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -12,7 +12,12 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.getHttpAdapter().getInstance().set('trust proxy', 1);
     app.enableCors({
-        origin: config.FRONTEND_URL,
+        // origin: config.FRONTEND_URL,
+        origin: [
+            'http://localhost:5173',
+            'https://task-flow-psi-ebon.vercel.app',
+            'https://task-flow-4rp3mipzd-ayushsharma72gs-projects.vercel.app',
+        ],
         credentials: true,
     });
     app.use(cookieParser());
